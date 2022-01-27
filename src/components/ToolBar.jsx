28046a0cde4a3ebcd4a8e1b5ec3345f7206d0c24
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-01-27 16:00:52 +0900

    navigationを修正

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -1,73 +1,32 @@
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import InsightsIcon from '@mui/icons-material/Insights';
-import RestaurantIcon from '@mui/icons-material/Restaurant';
-import SearchIcon from '@mui/icons-material/Search';
 import ListIcon from '@mui/icons-material/List';
 import NewspaperIcon from '@mui/icons-material/Newspaper';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import SearchIcon from '@mui/icons-material/Search';
 
 const ToolBar = (props) => {
+  const pages = ['search', 'recipe', 'list', 'news', 'score'];
+
   return (
-    <Box
+    <BottomNavigation
+      showLabels
       sx={{
-        display: 'flex',
         flexShrink: 0,
-        justifyContent: 'space-between',
         padding: 2,
         backgroundColor: 'lightblue',
-        color: 'white',
+      }}
+      onChange={(event, newValue) => {
+        props.onClick(pages[newValue]);
       }}
     >
-      <IconButton
-        size="large"
-        onClick={() => {
-          props.onClick('search');
-        }}
-      >
-        <SearchIcon fontSize="inherit" />
-      </IconButton>
-      <IconButton
-        size="large"
-        onClick={() => {
-          props.onClick('recipe');
-        }}
-      >
-        <RestaurantIcon fontSize="inherit" />
-      </IconButton>
-      <IconButton
-        size="large"
-        onClick={() => {
-          props.onClick('list');
-        }}
-      >
-        <ListIcon fontSize="inherit" />
-      </IconButton>
-      <IconButton
-        size="large"
-        onClick={() => {
-          props.onClick('add');
-        }}
-      >
-        <AddCircleOutlineIcon fontSize="inherit" />
-      </IconButton>
-      <IconButton
-        size="large"
-        onClick={() => {
-          props.onClick('news');
-        }}
-      >
-        <NewspaperIcon fontSize="inherit" />
-      </IconButton>
-      <IconButton
-        size="large"
-        onClick={() => {
-          props.onClick('score');
-        }}
-      >
-        <InsightsIcon fontSize="inherit" />
-      </IconButton>
-    </Box>
+      <BottomNavigationAction label="Search" icon={<SearchIcon />} />
+      <BottomNavigationAction label="Recipe" icon={<RestaurantIcon />} />
+      <BottomNavigationAction label="List" icon={<ListIcon />} />
+      <BottomNavigationAction label="News" icon={<NewspaperIcon />} />
+      <BottomNavigationAction label="Score" icon={<InsightsIcon />} />
+    </BottomNavigation>
   );
 };
 
