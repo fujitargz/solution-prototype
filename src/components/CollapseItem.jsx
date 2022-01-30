3a4c7ab46345587fb16d247072623323b5f20396
fujitargz <61151276+fujitargz@users.noreply.github.com>
2022-01-30 15:56:00 +0900

    リストの詳細表示機能を実装

---
+++ src/components/CollapseItem.jsx
@@ -0,0 +1,37 @@
+import { useReducer } from 'react';
+import Collapse from '@mui/material/Collapse';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const CollapseItem = (props) => {
+  const [isOpen, toggleIsOpen] = useReducer((prev) => !prev, false);
+  return (
+    <>
+    <ListItemButton key={props.item.id} onClick={toggleIsOpen}>
+      <ListItemText primary={props.item.name} secondary={props.item.bestBy || props.item.useBy} />
+      {isOpen ? <ExpandLess /> : <ExpandMore />}
+    </ListItemButton>
+      <Collapse in={isOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemText primary={`賞味期限 : ${props.item.bestBy}`} />
+          </ListItemButton>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemText primary={`消費期限 : ${props.item.useBy}`} />
+          </ListItemButton>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemText primary={`カテゴリ : ${props.item.category}`} />
+          </ListItemButton>
+          <ListItemButton sx={{ pl: 4 }}>
+            <ListItemText primary={`おすすめの保存方法 : ${props.item.bestWayToKeep}`} />
+          </ListItemButton>
+        </List>
+      </Collapse>
+      </>
+  );
+};
+
+export default CollapseItem;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,77 +2,128 @@ import { useState } from 'react';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import IconButton from '@mui/material/IconButton';
 import { List as MuiList } from '@mui/material';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import ListSubheader from '@mui/material/ListSubheader';
 import SortIcon from '@mui/icons-material/Sort';
 import Tooltip from '@mui/material/Tooltip';
+import CollapseItem from './CollapseItem';
 
 const initialIngredients = [
   {
     id: 0,
     name: 'じゃがいも',
-    limit: '2022-2-7',
+    createdAt: '2022-2-3',
+    category: '青果',
+    bestBy: '',
+    useBy: '2022-2-7',
+    bestWayToKeep: '風通しのよい冷暗所で保管'
   },
   {
     id: 1,
     name: 'にんじん',
-    limit: '2022-2-10',
+    createdAt: '2022-2-3',
+    category: '青果',
+    bestBy: '',
+    useBy: '2022-2-10',
+    bestWayToKeep: '新聞紙等で包み冷蔵庫で保管'
   },
   {
     id: 2,
     name: '豚バラ肉',
-    limit: '2022-2-4',
+    createdAt: '2022-2-3',
+    category: '肉',
+    bestBy: '',
+    useBy: '2022-2-4',
+    bestWayToKeep: '冷蔵庫で保管'
   },
   {
     id: 3,
     name: '白菜',
-    limit: '2022-2-11',
+    createdAt: '2022-2-3',
+    category: '青果',
+    bestBy: '',
+    useBy: '2022-2-11',
+    bestWayToKeep: '新聞紙等で包み冷蔵庫で保管'
   },
   {
     id: 4,
     name: 'もやし',
-    limit: '2022-2-3',
+    createdAt: '2022-2-3',
+    category: '青果',
+    bestBy: '2022-2-3',
+    useBy: '2022-2-4',
+    bestWayToKeep: '水に浸して冷蔵庫で保管'
   },
   {
     id: 5,
     name: '卵',
-    limit: '2022-2-20',
+    createdAt: '2022-2-20',
+    category: 'その他',
+    bestBy: '2022-2-20',
+    useBy: '',
+    bestWayToKeep: '冷蔵庫で保管'
   },
   {
     id: 6,
     name: 'ヨーグルト',
-    limit: '2022-2-16',
+    createdAt: '2022-2-3',
+    category: 'その他',
+    bestBy: '',
+    useBy: '2022-2-16',
+    bestWayToKeep: '冷蔵庫で保管'
   },
   {
     id: 7,
     name: '牛乳',
-    limit: '2022-2-28',
+    createdAt: '2022-2-3',
+    category: 'その他',
+    bestBy: '',
+    useBy: '2022-2-28',
+    bestWayToKeep: '冷蔵庫で保管'
   },
   {
     id: 8,
     name: '鶏もも肉',
-    limit: '2022-2-5',
+    createdAt: '2022-2-3',
+    category: '肉',
+    bestBy: '',
+    useBy: '2022-2-5',
+    bestWayToKeep: '冷蔵庫で保管'
   },
   {
     id: 9,
     name: '納豆',
-    limit: '2022-2-10',
+    createdAt: '2022-2-3',
+    category: 'その他',
+    bestBy: '2022-2-10',
+    useBy: '',
+    bestWayToKeep: '冷蔵庫で保管'
   },
   {
     id: 10,
     name: 'ソーセージ',
-    limit: '2022-2-19',
+    createdAt: '2022-2-3',
+    category: 'その他',
+    bestBy: '2022-2-19',
+    useBy: '',
+    bestWayToKeep: '冷蔵庫で保管'
   },
   {
     id: 11,
     name: 'みかん',
-    limit: '2022-3-10',
+    createdAt: '2022-2-3',
+    category: '青果',
+    bestBy: '',
+    useBy: '2022-3-10',
+    bestWayToKeep: '段ボールのまま冷暗所で保管'
   },
   {
     id: 12,
     name: 'ほうれん草',
-    limit: '2022-2-9',
+    createdAt: '2022-2-3',
+    category: '青果',
+    bestBy: '',
+    useBy: '2022-2-9',
+    bestWayToKeep: '新聞紙等で包み冷蔵庫で保管'
   },
 ];
 
@@ -81,9 +132,11 @@ const sortById = (a, b) => {
 };
 
 const sortByLimit = (a, b) => {
-  const aLimit = new Date(a.limit);
-  const bLimit = new Date(b.limit);
-  return aLimit - bLimit;
+  const aLimit = a.bestBy || a.useBy;
+  const bLimit = b.bestBy || b.useBy;
+  const aLimitDate = new Date(aLimit)
+  const bLimitDate = new Date(bLimit)
+  return aLimitDate - bLimitDate;
 };
 
 const List = () => {
@@ -116,13 +169,7 @@ const List = () => {
         </ListSubheader>
       }
     >
-      {ingredients.map((item) => {
-        return (
-          <ListItemButton key={item.id}>
-            <ListItemText primary={item.name} secondary={item.limit} />
-          </ListItemButton>
-        );
-      })}
+      {ingredients.map((item) =>  <CollapseItem item={item} />)}
     </MuiList>
   );
 };
